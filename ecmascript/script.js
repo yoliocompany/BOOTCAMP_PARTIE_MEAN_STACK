0f6d5c30383ffb8fd33135ce8d1288c78ab7c3cf
a-mean123 <mohamedamine.jebali@esen.tn>
2024-01-30 16:28:06 +0100

    seance 1

---
+++ ecmascript/script.js
@@ -0,0 +1,79 @@
+
+
+// function affiche() {
+
+//     for (var i = 0; i < 5; i++) {
+//         console.log(i);
+//         let x = 10;
+//         console.log(x);
+//     }
+
+//     console.log(i);
+
+// }
+
+// affiche()
+
+
+// default parameter value
+// const callUser = (name = 'Guest')=>{
+//     console.log( 'Hello ', name);
+// }
+
+
+// callUser('Amine');
+
+
+// rest parameter
+
+// const somme = ( ...tab )=>{
+
+//     console.log(tab);
+// }
+
+
+// somme( 'aaa', 'bbb', 'ccc' );
+
+
+// parameter spread
+
+// let users = [ 'Amine', 'Amira', 'Salim', 'Salma' ];
+
+// const display = ( a ,b ,c ,d )=>{
+    
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+//     console.log(d);
+
+// }
+
+// display(  ...users  );
+
+// let oldCars = [ '205', '404', '106' ];
+
+// let cars = [ ...oldCars ];
+
+
+// cars.push('105');
+
+
+// console.log(oldCars);
+// console.log(cars);
+
+
+// For of loop
+
+let oldCars = [ '205', '404', '106' ];
+
+for( let car of oldCars ){
+
+    console.log(car);
+
+}
+
+
+
+
+
+
